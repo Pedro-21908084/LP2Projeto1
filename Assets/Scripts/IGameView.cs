cf8ada2ef7bd031bd7e43ea021e1ae2658a61cbc
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-11-13 19:09:46 +0000

    Add basic functions and chnage name of script

--- Assets/Scripts/IGameView.cs
+++ Assets/Scripts/IGameView.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IView 
+public interface IGameView 
 {
 
     public void ShowMap();
     public void ShowTiles();
     public void ShowTileResources();
     public void ShowButtons();
+    public void ShowTileInfo();
 
 }
