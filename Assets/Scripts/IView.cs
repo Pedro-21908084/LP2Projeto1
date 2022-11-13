2d7654928ff5f7ca2d2ace4fe545b5a5f9ac07b0
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-11-13 18:45:28 +0000

    Create Script folder

---
+++ Assets/Scripts/IView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IView 
+{
+
+    public void ShowMap();
+    public void ShowTiles();
+    public void ShowTileResources();
+    public void ShowButtons();
+
+}
