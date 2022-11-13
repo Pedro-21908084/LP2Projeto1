99b84c680dbef894e31663d152802b64fcd38b88
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-11-13 19:41:39 +0000

    Add text and icon references to IGameView interface

--- Assets/Scripts/IGameView.cs
+++ Assets/Scripts/IGameView.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public interface IGameView 
 {
-
+    //Info Panel Text Refs
+    public TextMeshProUGUI TerrTypeTxt { get; set; }
+    public TextMeshProUGUI ResourcesTxt { get; set; }
+    public TextMeshProUGUI TotalCoinTxt { get; set; }
+    public TextMeshProUGUI TotalFoodTxt { get; set; }
+    
+    //Info Panel Icons
+    public Sprite PlantIcon { get; set;}
+    public Sprite AnimalsIcon { get; set; }
+    public Sprite MetalsIcon { get; set; }
+    public Sprite FossilFuelIcon { get; set; }
+    public Sprite LuxuryIcon { get; set; }
+    public Sprite PollutionIcon { get; set; }
+    
+    //Methods
     public void ShowMap();
     public void ShowTiles();
     public void ShowTileResources();
