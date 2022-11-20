7575bc553ed155ed6a89f05917e11c6d0ea18aac
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-11-20 18:28:16 +0000

    Add gameobject and button fields besides 2 more methods

--- Assets/Scripts/IGameView.cs
+++ Assets/Scripts/IGameView.cs
@@ -6,7 +6,12 @@ using TMPro;
 
 public interface IGameView 
 {
-    //Info Panel Text Refs
+    //GameObjects
+    public GameObject TileInfoPanel { get; set;}
+    public GameObject MapLegend { get; set;}
+    public GameObject PauseMenu { get; set; }
+
+    //Info Panel Texts
     public TextMeshProUGUI TerrTypeTxt { get; set; }
     public TextMeshProUGUI ResourcesTxt { get; set; }
     public TextMeshProUGUI TotalCoinTxt { get; set; }
@@ -19,12 +24,21 @@ public interface IGameView
     public Sprite FossilFuelIcon { get; set; }
     public Sprite LuxuryIcon { get; set; }
     public Sprite PollutionIcon { get; set; }
-    
+
+    //Buttons
+    public Button forTheFuture1 { get; set; }
+    public Button forTheFuture2 { get; set; }
+    public Button forTheFuture3 { get; set; }
+    public Button forTheFuture4 { get; set; }
+    public Button forTheFuture5 { get; set; }
+
     //Methods
     public void ShowMap();
     public void ShowTiles();
     public void ShowTileResources();
     public void ShowButtons();
     public void ShowTileInfo();
+    public void ShowMapLegend();
+    public void ShowPauseMenu();
 
 }
