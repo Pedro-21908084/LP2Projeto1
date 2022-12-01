383f211fe69c59f515ea613f8034aa8bcf7b8de6
Pedro-21908084 <pm_rf@hotmail.com>
2022-12-01 19:55:49 +0000

    set up tests

--- Assets/Scripts/IGameView.cs
+++ Assets/Scripts/IGameView.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public interface IGameView 
+public interface IGameView
 {
     //Methods
     public void ShowMap(Tile[][] map, GameData gameData);
