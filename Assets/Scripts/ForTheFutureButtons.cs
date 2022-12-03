dc02969678de6324d9c06b03fa29fdca859b0731
Pedro-21908084 <pm_rf@hotmail.com>
2022-12-03 22:35:27 +0000

    create script fot the future buttons

---
+++ Assets/Scripts/ForTheFutureButtons.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForTheFutureButtons : MonoBehaviour
+{
+    [SerializeField] private Controller controller;
+    [SerializeField] private MenusController menusController;
+    [SerializeField] private string text;
+
+    public virtual void ButtonMethod()
+    {
+        menusController.CloseFutureMenu();
+    }
+}
